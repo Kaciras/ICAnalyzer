607f33e15729c09992e82870d4e507192ae06a69
Kaciras <Kaciras@outlook.com>
2020-09-15 12:31:56 +0800

    rename files

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
 	install_requires=[
 		"fire",
 		"colorama",
-		"PIL",
 		"opencv-python",
 		"matplotlib",
 		"numpy",
--- src/cwebp.py
+++ src/cwebp.py
@@ -1,9 +1,7 @@
 import asyncio
 from asyncio.subprocess import create_subprocess_exec, PIPE
-from pathlib import Path
 
-from _common import hrsize
-from img_metrics import ImageCodecAnalyzer, get_mux, ImageCodecPlot
+from .metrics import ImageCodecAnalyzer, get_mux, ImageCodecPlot
 
 # 保存编码器输出的临时文件，可以搞成内存文件系统加点速
 TEMP_DIR = "R:\\"
@@ -41,7 +39,7 @@ class CWebPAnalyzer:
 
 		return plot.draw_lines(xtricks)
 
-# np.savetxt("rs.csv", results, delimiter=",")
+	# np.savetxt("rs.csv", results, delimiter=",")
 
 	async def quality(self, range_=range(0, 101)):
 		argslist = [("-sharp_yuv", "-q", str(v)) for v in range_]
@@ -110,40 +108,7 @@ async def analyze(file):
 	# analyzer.lossless_mode()
 	# analyzer.near_lossless()
 
-def collect(files, map_func):
-	rsize = wsize = count = 0
-
-	for file in files:
-		target = map_func(file)
-
-		if target.is_file():
-			rsize += file.stat().st_size
-			wsize += target.stat().st_size
-			count += 1
-
-	return count, rsize, wsize
-
-
-def statistic():
-	webps = Path(r"G:\blog\cache\webp")
-	files = Path(r"G:\blog\image").iterdir()
-
-	def map_webp(oigin):
-		return webps.joinpath(oigin.name).with_suffix(".webp")
-
-	count, rsize, wsize = collect(files, map_webp)
-	print(f"{count=}, rsize={hrsize(rsize)}, wsize={hrsize(wsize)}, "
-		  f"压缩率={wsize / rsize * 100:.2f}%")
-
-
-# 75,021
-# 3,935
-# 231,180 = 310136
-#
-# 16,160
-# 33,449 =
 
 if __name__ == '__main__':
 	asyncio.get_event_loop().run_until_complete(analyze(
 		r"G:\blog\image\6f6a94d94f9eb1a25faaa68ea3f8565ad09a80b4458bcdbb6bea9ed95f5a3df0.png"))
-# statistic()
