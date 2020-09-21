66b2828c3330ac0ceee355d7c7ccc44d13f23882
Kaciras <Kaciras@outlook.com>
2020-09-21 14:42:39 +0800

    add typescript & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module"
+	},
+	plugins: [
+		"@typescript-eslint"
+	],
+	rules: {
+		"indent": ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		"quotes": ["error", "double"],
+		"semi": ["error", "always"],
+		"@typescript-eslint/explicit-module-boundary-types": ["off"],
+	}
+};
