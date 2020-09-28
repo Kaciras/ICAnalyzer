eb40f887261bb4f9a01a611916835737f1588c6c
Kaciras <Kaciras@outlook.com>
2020-09-28 15:25:16 +0800

    (WIP) use react hooks

--- web/App.tsx
+++ web/App.tsx
@@ -1,59 +1,9 @@
-import React, { useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import ImageDifference from "./component/ImageDifference";
 import { createAVIFEncoder, createWebPEncoder } from "./encoding";
-import { drawChart } from "./chart";
 import { WebPEncodeOptions } from "./worker/webp-encoder";
 import { AVIFEncodeOptions, Subsample } from "./worker/avif-encoder";
-import { isSupportAVIF } from "./utils";
-
-const fileInput = document.getElementById("file") as HTMLInputElement;
-fileInput.oninput = loadFile;
-
-const rangeInput = document.getElementById("range") as HTMLInputElement;
-
-const canvasP = document.getElementById("preview") as HTMLCanvasElement;
-const canvasD = document.getElementById("diff") as HTMLCanvasElement;
-const ctxP = canvasP.getContext("2d")!;
-const ctxD = canvasD.getContext("2d")!;
-
-async function loadFile() {
-	const file = fileInput.files![0];
-	const bitmap = await createImageBitmap(file);
-	const { width, height } = bitmap;
-
-	canvasP.width = width;
-	canvasP.height = height;
-	canvasD.width = width;
-	canvasD.height = height;
-
-	ctxP.drawImage(bitmap, 0, 0);
-	const canvasData = ctxP.getImageData(0, 0, width, height);
-
-	if (!(await isSupportAVIF())) {
-		return alert("Your browser does not support avif image.");
-	}
-	return showResult(file, await encodeAvif(canvasData));
-
-	// console.info(`Origin: ${file.size}`);
-	// console.info(`WebP: ${webp.byteLength}`);
-	// console.info(`Compress ratio: ${webp.byteLength / event.target.files[0].size * 100}%`);
-}
-
-function encode(image: ImageData) {
-	const optionsList = new Array<WebPEncodeOptions>(101);
-	for (let i = 0; i < 101; i++) {
-		optionsList[i] = { quality: i };
-	}
-
-	const progress = document.getElementById("progress") as HTMLProgressElement;
-	progress.value = 0;
-	progress.max = optionsList.length;
-
-	const encoder = createWebPEncoder();
-	encoder.onProgress = value => progress.value = value;
-
-	return encoder.encode(image, optionsList).start();
-}
+import Chart from "./component/Chart";
 
 function encodeAvif(image: ImageData) {
 	const optionsList = new Array<AVIFEncodeOptions>();
@@ -71,7 +21,7 @@ function encodeAvif(image: ImageData) {
 		};
 	}
 
-	rangeInput.max = "31";
+	// rangeInput.max = "31";
 
 	const progress = document.getElementById("progress") as HTMLProgressElement;
 	progress.value = 0;
@@ -83,75 +33,138 @@ function encodeAvif(image: ImageData) {
 	return encoder.encode(image, optionsList).start();
 }
 
-async function showResult(file: File, encodedFiles: Uint8Array[]) {
-	const rawUrl = URL.createObjectURL(file);
-	const parent = document.getElementById("blend")!;
-	parent.style.backgroundImage = `url("${rawUrl}")`;
-
-	const brightnessInput = document.getElementById("brightness") as HTMLInputElement;
-	brightnessInput.oninput = () => parent.style.setProperty("--brightness", brightnessInput.value + "%");
-
-	const label = document.getElementById("range-label") as HTMLLabelElement;
-
-	const bitmaps: ImageBitmap[] = [];
-	for (const data of encodedFiles) {
-		const blob = new Blob([data], { type: "image/avif" });
-		bitmaps.push(await createImageBitmap(blob));
-	}
-
-	const chart = drawChart({
-		title: {
-			text: "Quality (-q)"
-		},
-		tooltip: {
-			trigger: "axis",
-		},
-		legend: {
-			data: ["Compression Ratio"]
-		},
-		xAxis: {
-			data: encodedFiles.map((_, i) => i)
-		},
-		yAxis: {},
-		series: [{
-			name: "Compression Ratio %",
-			type: "line",
-			data: encodedFiles.map(({ length }) => length / file.size * 100),
-		}],
-	});
-
-	function show(i: number) {
-		label.textContent = `Quality (-q) = ${i}`;
-		ctxP.drawImage(bitmaps[i], 0, 0);
-		ctxD.drawImage(bitmaps[i], 0, 0);
-		// chart.dispatchAction({ type: "showTip", x: i, y: 0,position: ["50%", "50%"] });
-	}
-
-	rangeInput.valueAsNumber = 75;
-	rangeInput.oninput = () => show(rangeInput.valueAsNumber);
-
-	show(parseInt(rangeInput.max));
-}
-
 export default function App() {
 	const [original, setOriginal] = useState<File>();
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [width, setWidth] = useState(0);
+	const [height, setHeight] = useState(0);
+
+	const [max, setMax] = useState(1);
+	const [progress, setProgress] = useState(0);
+
+	const [results, setResults] = useState<ImageBitmap[]>([]);
+	const [brightness, setBrightness] = useState(100);
+	const [optimized, setOptimized] = useState<ImageBitmap>();
+
+	const [index, setIndex] = useState(0);
+
+	async function loadFile(event: FormEvent<HTMLInputElement>) {
+		const file = event.currentTarget.files![0];
+		const canvas = canvasRef.current!;
+
+		const bitmap = await createImageBitmap(file);
+		const { width, height } = bitmap;
+		setOriginal(file);
+		setWidth(width);
+		setHeight(height);
+		canvas.width = width;
+		canvas.height = height;
+
+		const ctx = canvas.getContext("2d")!;
+		ctx.drawImage(bitmap, 0, 0);
+		const canvasData = ctx.getImageData(0, 0, width, height);
+
+		// if (!(await isSupportAVIF())) {
+		// 	return alert("Your browser does not support avif image.");
+		// }
+
+		return showResult(file, await encode(canvasData));
+	}
+
+	function encode(image: ImageData) {
+		const optionsList = new Array<WebPEncodeOptions>(101);
+		for (let i = 0; i < 101; i++) {
+			optionsList[i] = { quality: i };
+		}
+
+		setMax(optionsList.length);
+		setProgress(0);
+
+		const encoder = createWebPEncoder();
+		encoder.onProgress = setProgress;
+		return encoder.encode(image, optionsList).start();
+	}
+
+	async function showResult(file: File, encodedFiles: Uint8Array[]) {
+
+		const bitmaps: ImageBitmap[] = [];
+		for (const data of encodedFiles) {
+			const blob = new Blob([data], { type: "image/webp" });
+			bitmaps.push(await createImageBitmap(blob));
+		}
+		setResults(bitmaps);
+
+		// const chart = drawChart({
+		// 	title: {
+		// 		text: "Quality (-q)"
+		// 	},
+		// 	tooltip: {
+		// 		trigger: "axis",
+		// 	},
+		// 	legend: {
+		// 		data: ["Compression Ratio"]
+		// 	},
+		// 	xAxis: {
+		// 		data: encodedFiles.map((_, i) => i)
+		// 	},
+		// 	yAxis: {},
+		// 	series: [{
+		// 		name: "Compression Ratio %",
+		// 		type: "line",
+		// 		data: encodedFiles.map(({ length }) => length / file.size * 100),
+		// 	}],
+		// });
+
+
+		setIndex(75);
+		setOptimized(bitmaps[75]);
+	}
+
+	function handleIndexChange(e: FormEvent<HTMLInputElement>) {
+		const i = e.currentTarget.valueAsNumber;
+		setIndex(i);
+		setOptimized(results[i]);
+		canvasRef.current!.getContext("2d")!.drawImage(results[i], 0, 0);
+		// chart.dispatchAction({ type: "showTip", x: i, y: 0,position: ["50%", "50%"] });
+	}
 
 	return (
 		<>
 			<section>
-				<ImageDifference original={original} optimized={undefined}/>
-				<canvas></canvas>
+				<ImageDifference
+					original={original}
+					optimized={optimized}
+					width={width}
+					height={height}
+					brightness={brightness}
+				/>
+				<canvas ref={canvasRef}/>
 			</section>
 			<section>
-				<div id="metrics"></div>
+				<Chart/>
 				<form id="form">
-					<input id="file" type="file" accept="image/*"/>
+					<input type="file" accept="image/*" onChange={loadFile}/>
 
-					<label id="range-label" htmlFor="range">Quality (-q)</label>
-					<input id="range" type="range" min="0" step="1"/>
+					<label htmlFor="range">Quality (-q) {index}</label>
+					<input
+						id="range"
+						type="range"
+						value={index}
+						min={0}
+						step={1}
+						onChange={handleIndexChange}
+					/>
 
-					<input id="brightness" type="number" min="100" step="20" value="100"/>
-					<progress id="progress"></progress>
+					<input
+						id="brightness"
+						type="number"
+						min="100"
+						step="50"
+						value="100"
+						onChange={e => setBrightness(e.currentTarget.valueAsNumber)}
+					/>
+
+					<progress id="progress" value={progress} max={max}/>
 				</form>
 			</section>
 		</>
--- web/component/ImageDifference.tsx
+++ web/component/ImageDifference.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { CSSProperties, useEffect, useRef, useState } from "react";
 import style from "./imageDifference.scss";
 
 export interface ImageDiffProps {
 	original?: Blob;
-	optimized?: ImageData;
+	optimized?: ImageBitmap;
+	width: number;
+	height: number;
+	brightness: number;
 }
 
 export default function ImageDifference(props: ImageDiffProps) {
+	const canvasEl = useRef<HTMLCanvasElement>(null);
+	const [wrapperStyle, setWrapperStyle] = useState<CSSProperties>({});
+
+	useEffect(() => {
+		if (!props.original) {
+			return;
+		}
+		const rawUrl = URL.createObjectURL(props.original);
+
+		const canvas = canvasEl.current!;
+		canvas.width = props.width;
+		canvas.height = props.height;
+
+		setWrapperStyle({
+			backgroundImage: `url("${rawUrl}")`,
+		});
+		return () => URL.revokeObjectURL(rawUrl);
+	}, [props.original]);
+
+	useEffect(() => {
+		if (!props.optimized) {
+			return;
+		}
+		const ctx = canvasEl.current!.getContext("2d");
+		ctx!.drawImage(props.optimized!, 0, 0);
+	}, [props.optimized]);
+
 	return (
-		<div className={style.container}>
-			<canvas className={style.canvas}/>
+		<div className={style.container} style={wrapperStyle}>
+			<canvas className={style.canvas} ref={canvasEl}/>
 		</div>
 	);
 }
