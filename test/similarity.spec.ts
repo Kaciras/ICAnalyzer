78ce6bd5c00684c4cd524c918f794106ed6272b0
Kaciras <Kaciras@outlook.com>
2020-10-01 11:29:46 +0800

    fix tests

--- test/similarity.spec.ts
+++ test/similarity.spec.ts
@@ -1,5 +1,6 @@
 import { join } from "path";
 import sharp from "sharp";
+import fs from "fs";
 import { butteraugli, getPSNR } from "../web/similarity";
 
 function fixture(name: string) {
@@ -7,38 +8,32 @@ function fixture(name: string) {
 }
 
 async function readImage(name: string) {
-	return await sharp(fixture("image.jpg"))
+	return await sharp(fixture(name))
 		.raw()
 		.toBuffer({ resolveWithObject: true });
 }
 
-it("should get butteraugli source", async () => {
+it("should get butteraugli source & heatMap", async () => {
 	const { data, info } = await readImage("image.jpg");
 	const dataB = (await readImage("image.webp")).data;
+	const expectdHeatMap = (await readImage("heatMap.png")).data;
+
+	fs.writeFileSync("jpeg.data", data);
+	fs.writeFileSync("webp.data", dataB);
 
 	const { width, height } = info;
 	const [source, heatMap] = await butteraugli({ dataA: data, dataB, width, height });
 
-	expect(source).toBe(5);
-
-	return sharp(Buffer.from(heatMap), { raw: { channels: 3, width, height } })
-		.png()
-		.toFile("heatmap.png");
+	expect(source).toBeCloseTo(3.297, 2);
+	expect(Buffer.from(heatMap)).toStrictEqual(expectdHeatMap);
 });
 
 it("should get PSNR", async () => {
-	const { data, info } = await sharp(fixture("image.jpg"))
-		// .ensureAlpha()
-		.raw()
-		.toBuffer({ resolveWithObject: true });
-
-	const dataB = await sharp(fixture("image.webp"))
-		// .ensureAlpha()
-		.raw()
-		.toBuffer();
+	const { data, info } = await readImage("image.jpg");
+	const dataB = (await readImage("image.webp")).data;
 
 	const { width, height } = info;
 	const psnr = await getPSNR({ dataA: data, dataB, width, height });
 
-	expect(psnr).toBeCloseTo(40.238, 2);
+	expect(psnr).toBeCloseTo(35.778, 2);
 });
