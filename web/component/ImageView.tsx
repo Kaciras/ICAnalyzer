88bcb2f391b0e82de4e73da50dfbb7e64ff84815
Kaciras <Kaciras@outlook.com>
2020-10-17 17:56:02 +0800

    ImageView: add drag move support

--- web/component/ImageView.tsx
+++ web/component/ImageView.tsx
@@ -25,6 +25,44 @@ interface ImageViewCSS extends CSSProperties {
 	"--brightness": string;
 }
 
+type PointerMoveHandler = (dx: number, dy: number) => void;
+
+function watchMouseMove(baseEvent: MouseEvent, handler: PointerMoveHandler) {
+	const basePageX = baseEvent.pageX;
+	const basePageY = baseEvent.pageY;
+
+	function handleMove(event: MouseEvent) {
+		handler(event.pageX - basePageX, event.pageY - basePageY);
+	}
+
+	function handleEnd(event: Event) {
+		event.preventDefault();
+		document.removeEventListener("mousemove", handleMove);
+		document.removeEventListener("mouseup", handleEnd);
+	}
+
+	document.addEventListener("mousemove", handleMove);
+	document.addEventListener("mouseup", handleEnd);
+}
+
+function watchTouchMove(baseEvent: TouchEvent, handler: PointerMoveHandler) {
+	const basePointer = baseEvent.touches[0];
+
+	function handleMove(event: TouchEvent) {
+		const touch = event.touches[0];
+		handler(touch.pageX - basePointer.pageX, touch.pageY - basePointer.pageY);
+	}
+
+	function handleEnd(event: Event) {
+		event.preventDefault();
+		document.removeEventListener("touchmove", handleMove);
+		document.removeEventListener("touchend", handleEnd);
+	}
+
+	document.addEventListener("touchmove", handleMove);
+	document.addEventListener("touchend", handleEnd);
+}
+
 export default function ImageView(props: Props) {
 	const { original, width, height, optimized, heatMap } = props;
 
@@ -32,12 +70,12 @@ export default function ImageView(props: Props) {
 	const [brightness, setBrightness] = useState(100);
 
 	const [zoom, setZoom] = useState(1);
-	const [move, setMove] = useState({ x: 0, y: 0 });
+	const [offset, setOffset] = useState({ x: 0, y: 0 });
 
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	function refreshCanvas() {
-		if(!optimized) {
+		if (!optimized) {
 			return; // TODO
 		}
 		const ctx = canvasRef.current!.getContext("2d");
@@ -80,12 +118,23 @@ export default function ImageView(props: Props) {
 		setZoom(zoom * scaleDiff);
 	}
 
+	function offsetUpdater() {
+		const baseX = offset.x;
+		const baseY = offset.y;
+
+		return (dx: number, dy: number) => {
+			const x = baseX + dx;
+			const y = baseY + dy;
+			setOffset({ x, y });
+		};
+	}
+
 	const wrapperCss: ImageViewCSS = {
 		width, height,
 		"--original": `url("${original}")`,
 		"--scale": zoom,
-		"--x": move.x + "px",
-		"--y": move.y + "px",
+		"--x": offset.x + "px",
+		"--y": offset.y + "px",
 		"--brightness": `${brightness}%`,
 	};
 
@@ -95,6 +144,8 @@ export default function ImageView(props: Props) {
 		<section
 			className={Styles.container}
 			onWheel={handleWheel}
+			onMouseDown={e => watchMouseMove(e.nativeEvent, offsetUpdater())}
+			onTouchStart={e => watchTouchMove(e.nativeEvent, offsetUpdater())}
 		>
 			<aside className={Styles.inputs}>
 				<ImageViewTab target={ViewType.Original}>Original</ImageViewTab>
