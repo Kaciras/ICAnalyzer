db05d92b6811d4503e946089bfe97c56f7492192
Kaciras <Kaciras@outlook.com>
2020-10-25 16:42:04 +0800

    SelectFilePanel: add drag & drop support

--- web/component/SelectFilePanel.tsx
+++ web/component/SelectFilePanel.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, Dispatch } from "react";
+import React, { ChangeEvent, Dispatch, DragEvent, useRef, useState } from "react";
+import clsx from "clsx";
 import ImageIcon from "bootstrap-icons/icons/image.svg";
 import Styles from "./SelectFilePanel.scss";
 
@@ -7,6 +8,8 @@ interface Props {
 }
 
 export default function SelectFilePanel(props: Props) {
+	const enterCount = useRef(0);
+	const [dragging, setDragging] = useState(false);
 
 	function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
 		props.onFileChange(event.currentTarget.files![0]);
@@ -16,14 +19,42 @@ export default function SelectFilePanel(props: Props) {
 		props.onFileChange(event.currentTarget.value);
 	}
 
+	function handleDrag(isEnter: boolean) {
+		if (isEnter) {
+			enterCount.current++;
+		} else {
+			enterCount.current--;
+		}
+		const active = enterCount.current > 0;
+		if (dragging !== active) {
+			setDragging(active);
+		}
+	}
+
+	function handleDrop(event: DragEvent) {
+		event.preventDefault();
+		const { items } = event.dataTransfer;
+		const file = items[0].getAsFile();
+		file && props.onFileChange(file);
+	}
+
 	return (
 		<form className={Styles.form}>
-			<label className={Styles.uploadFile} tabIndex={0}>
-				<div className={Styles.icon}>
-					<ImageIcon/>
-				</div>
-				<div className={Styles.text}>
-					Drag & drop
+			<label
+				className={clsx(Styles.uploadFile, { [Styles.dragging]: dragging })}
+				tabIndex={0}
+				onDragEnter={() => handleDrag(true)}
+				onDragOver={e => e.preventDefault()}
+				onDrop={handleDrop}
+				onDragLeave={() => handleDrag(false)}
+			>
+				<div>
+					<span className={Styles.icon}>
+						<ImageIcon/>
+					</span>
+					<span className={Styles.text}>
+						Drag & drop
+					</span>
 				</div>
 				<div className={Styles.text}>
 					Or select an image
@@ -37,9 +68,9 @@ export default function SelectFilePanel(props: Props) {
 				/>
 			</label>
 			<label>
-				<span>Or image url</span>
+				<p>Or image url:</p>
 				<input
-					className="text-box"
+					className={Styles.textInput}
 					name="url"
 					onChange={handleUrlChange}
 				/>
