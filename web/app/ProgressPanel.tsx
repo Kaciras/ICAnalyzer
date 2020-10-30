e33ba1aa26880699a5aac4b3fa46483fcfd2c213
Kaciras <Kaciras@outlook.com>
2020-10-30 15:45:49 +0800

    commit missing changes

--- web/app/ProgressPanel.tsx
+++ web/app/ProgressPanel.tsx
@@ -3,7 +3,7 @@ import { MyButton } from "../ui";
 import Styles from "./ProgressPanel.scss";
 import { debounce } from "../utils";
 
-const formatter = new Intl.RelativeTimeFormat("en", { maximumFractionDigits: 1});
+const formatter = new Intl.RelativeTimeFormat("en");
 
 interface ProgressPanelProps {
 	max: number;
@@ -20,22 +20,34 @@ export default function ProgressPanel(props: ProgressPanelProps) {
 	const { value, max, onCancel } = props;
 
 	const [start] = useState(() => performance.now());
-	const ms = (max - value) / value * (performance.now() - start);
+	const debounced = useMemo(() => debounce(500, calcTime), []);
 
+	const ms = debounced(start, value, max);
 	const remaining = Number.isNaN(ms)
 		? "----"
-		: formatter.format(ms / 1000, "seconds");
+		: formatter.format(~~(ms / 1000), "seconds");
 
 	return (
 		<>
 			<h1>Encoding...</h1>
-			<div className={Styles.textProgress}>
-				<span>{value}/{max}</span>
-				<span>Remaining: {remaining}</span>
-			</div>
-			<progress value={value} max={max}/>
-			<div className={Styles.buttons}>
-				<MyButton onClick={onCancel}>Cancel</MyButton>
+			<div className={Styles.content}>
+				<div className={Styles.textProgress}>
+					<span>{value}/{max}</span>
+					<span>Remaining: {remaining}</span>
+				</div>
+				<progress
+					value={value}
+					max={max}
+					className={Styles.progress}
+				/>
+				<div className={Styles.buttons}>
+					<MyButton
+						color="second"
+						onClick={onCancel}
+					>
+						Cancel
+					</MyButton>
+				</div>
 			</div>
 		</>
 	);
