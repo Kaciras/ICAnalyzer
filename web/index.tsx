86ee2eccfee4a36cb55ed03d6ca1e9a8cc858f42
Kaciras <Kaciras@outlook.com>
2020-11-06 18:15:10 +0800

    alert if browser is not supported

--- web/index.tsx
+++ web/index.tsx
@@ -3,4 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./app/App";
 
+function detectBrowserSupport() {
+	try {
+		new File([], "");
+	} catch (_) {
+		return false;
+	}
+	if (!CSS.supports("mix-blend-mode: difference")) {
+		return false;
+	}
+	return "AbortController" in window;
+}
+
+if (!detectBrowserSupport()) {
+	alert("ICAnalyze does not support this browser, please switch to a modern one");
+}
+
 ReactDOM.render(<App/>, document.getElementById("root"));
