50bac8873e0c5d61ae524a4a96092648bc675382
Kaciras <Kaciras@outlook.com>
2020-11-13 22:43:41 +0800

    add sentry

--- web/index.tsx
+++ web/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "./index.scss";
 import App from "./app/App";
 
@@ -18,5 +20,13 @@ function detectBrowserSupport() {
 if (!detectBrowserSupport()) {
 	alert("ICAnalyze does not support this browser, please switch to a modern one");
 }
+
+Sentry.init({
+	dsn: "https://d101904fac2c486ea6f8563c2cf054af@o253601.ingest.sentry.io/5499968",
+	integrations: [
+		new Integrations.BrowserTracing(),
+	],
+	tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App/>, document.getElementById("root"));
