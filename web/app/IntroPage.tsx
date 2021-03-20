ff0bfe27375b5e86774741582ed1df1970fd3787
Kaciras <Kaciras@protonmail.com>
2021-03-20 16:10:17 +0800

    add banner

--- web/app/IntroPage.tsx
+++ web/app/IntroPage.tsx
@@ -1,6 +1,7 @@
 import { Button, IconButton } from "../ui";
-import style from "./AnalyzePage.scss";
+import styles from "./IntroPage.scss";
 import GitHubIcon from "../assets/github-logo.svg";
+import banner from "../assets/intro-banner.png";
 
 interface IntroPageProps {
 	onStart: () => void;
@@ -11,18 +12,34 @@ export default function IntroPage(props: IntroPageProps) {
 
 	return (
 		<div>
-			<section>
-				<Button onClick={onStart}>Try it!</Button>
+			<section className={styles.header}>
+				<img className={styles.banner} alt="banner" src={banner}/>
 
-				<IconButton
-					href="https://github.com/Kaciras/ICAnalyze"
-					className={style.iconButton}
-				>
-					<GitHubIcon/>
-					Source Code
-				</IconButton>
+				<div>
+					<h1 className={styles.title}>ICAnalyze</h1>
+					<Button
+						className={styles.bigButton}
+						onClick={onStart}
+					>
+						Try it!
+					</Button>
+					<IconButton
+						href="https://github.com/Kaciras/ICAnalyze"
+						className={styles.bigButton}
+					>
+						<GitHubIcon/>
+						Source Code
+					</IconButton>
+				</div>
+
+				<div className={styles.unsplash}>
+					Photo by
+					<a className={styles.link} href="https://unsplash.com/@veverkolog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dušan veverkolog</a>
+					on
+					<a className={styles.link} href="https://unsplash.com/s/photos/eagle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+				</div>
 			</section>
-			<section>
+			<section className={styles.section}>
 				TODO
 			</section>
 		</div>
