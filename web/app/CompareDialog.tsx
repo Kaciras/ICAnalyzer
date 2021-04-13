a1cf96c53ed382f1fc21f85e5b15ce73c453fa26
Kaciras <Kaciras@protonmail.com>
2021-04-13 23:14:40 +0800

    avoid redundant redraw by wrap PreviewBox with React.memo

--- web/app/CompareDialog.tsx
+++ web/app/CompareDialog.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, useEffect, useRef, useState } from "react";
+import { Dispatch, memo, useState } from "react";
 import CloseIcon from "bootstrap-icons/icons/x.svg";
 import { decode } from "../decode";
 import { Button, Dialog, FileDrop } from "../ui";
@@ -6,9 +6,11 @@ import { InputImage } from "./index";
 import { CompareData } from "./CompareSession";
 import styles from "./CompareDialog.scss";
 
+type InputState = InputImage | undefined;
+
 interface UploadBoxProps {
 	name: string;
-	onChange: Dispatch<InputImage>;
+	onChange: Dispatch<InputState>;
 	onError: Dispatch<string | undefined>;
 }
 
@@ -39,23 +41,16 @@ function UploadBox(props: UploadBoxProps) {
 interface PreviewBoxProps {
 	value: InputImage;
 	name: string;
-	onReset: () => void;
+	onChange: Dispatch<InputState>;
 }
 
-function PreviewBox(props: PreviewBoxProps) {
-	const { value, name, onReset } = props;
+const PreviewBox = memo((props: PreviewBoxProps) => {
+	const { value, name, onChange } = props;
 	const { file, data } = value;
 
-	const canvas = useRef<HTMLCanvasElement>(null);
-
-	function draw() {
-		if (value) {
-			const ctx = canvas.current!.getContext("2d");
-			ctx?.putImageData(value.data, 0, 0);
-		}
+	function drawImage(el: HTMLCanvasElement) {
+		el?.getContext("2d")!.putImageData(data, 0, 0);
 	}
-
-	useEffect(draw, [value]);
 
 	return (
 		<section className={styles.section}>
@@ -65,23 +60,23 @@ function PreviewBox(props: PreviewBoxProps) {
 					className={styles.reset}
 					type="text"
 					title="Reset"
-					onClick={onReset}
+					onClick={() => onChange(undefined)}
 				>
 					<CloseIcon/>
 				</Button>
 			</div>
 			<canvas
 				className={styles.canvas}
-				ref={canvas}
+				ref={drawImage}
 				width={data.width}
 				height={data.height}
 			/>
 			<div className={styles.info}>{file.name}</div>
 		</section>
 	);
-}
+});
 
-type InputState = InputImage | undefined;
+PreviewBox.displayName = "PreviewBox";
 
 export interface CompareDialogProps {
 	data?: CompareData;
@@ -100,9 +95,9 @@ export default function CompareDialog(props: CompareDialogProps) {
 		onAccept({ origin: origin!, output: output! });
 	}
 
-	function createSection(name: string, value: InputState, setValue: Dispatch<InputState>) {
+	function section(name: string, value: InputState, setValue: Dispatch<InputState>) {
 		return value
-			? <PreviewBox name={name} value={value} onReset={() => setValue(undefined)}/>
+			? <PreviewBox name={name} value={value} onChange={setValue}/>
 			: <UploadBox name={name} onChange={setValue} onError={setError}/>;
 	}
 
@@ -111,8 +106,8 @@ export default function CompareDialog(props: CompareDialogProps) {
 	return (
 		<Dialog onClose={onCancel}>
 			<div className={styles.body}>
-				{createSection("Origin image", origin, setOrigin)}
-				{createSection("Changed image", output, setOutput)}
+				{section("Origin image", origin, setOrigin)}
+				{section("Changed image", output, setOutput)}
 			</div>
 			<div className="dialog-actions">
 				<span className={styles.error}>{error}</span>
