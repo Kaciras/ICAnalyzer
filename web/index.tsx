92ac0c841364b572d2b7f55e7c6fb312b02f7aca
Kaciras <Kaciras@protonmail.com>
2021-10-04 20:50:34 +0800

    disable sentry on unsupported browser

--- web/index.tsx
+++ web/index.tsx
@@ -17,13 +17,14 @@ function detectBrowserSupport() {
 	return "AbortController" in window;
 }
 
-if (!detectBrowserSupport()) {
+const isSupported = detectBrowserSupport();
+if (!isSupported) {
 	alert("ICAnalyzer does not support this browser, please switch to a modern one");
 }
 
 // Can't use { name, version } = packageJson, because webpack doesn't support tree shaking for object destructuring.
 // SENTRY_DSN is only defined on web build.
-if (process.env.SENTRY_DSN !== null) {
+if (isSupported && process.env.SENTRY_DSN !== null) {
 	Sentry.init({
 		dsn: process.env.SENTRY_DSN,
 		integrations: [
