bf526de9ef8fa749c5d9e18e85fff9babc30498f
Kaciras <Kaciras@protonmail.com>
2023-12-27 13:52:15 +0800

    remove deprecated `@sentry/tracing`

--- web/index.tsx
+++ web/index.tsx
@@ -1,18 +1,17 @@
 import { createRoot } from "react-dom/client";
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import packageJson from "../package.json";
 import "./index.scss";
 import App from "./app";
 
-// SENTRY_DSN is only defined on the CI.
+// SENTRY_DSN is defined on the CI.
 if (window.__isSupport__ && process.env.SENTRY_DSN !== null) {
 	Sentry.init({
 		dsn: process.env.SENTRY_DSN,
 		integrations: [
-			new Integrations.BrowserTracing(),
+			new Sentry.BrowserTracing(),
 		],
-		tracesSampleRate: 0.2,
+		tracesSampleRate: 1.0,
 		release: `${packageJson.name}@${packageJson.version}`,
 	});
 }
