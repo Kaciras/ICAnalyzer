85f041bf7cbf2e721266e62df513b23f7ace9ec2
Kaciras <Kaciras@protonmail.com>
2024-05-18 07:16:37 +0800

    migrate Sentry v9

--- web/index.tsx
+++ web/index.tsx
@@ -9,7 +9,7 @@ if (window.__isSupport__ && process.env.SENTRY_DSN !== null) {
 	Sentry.init({
 		dsn: process.env.SENTRY_DSN,
 		integrations: [
-			new Sentry.BrowserTracing(),
+			Sentry.browserTracingIntegration()
 		],
 		tracesSampleRate: 1.0,
 		release: `${packageJson.name}@${packageJson.version}`,
